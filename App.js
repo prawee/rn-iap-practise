25060e49c56f362602f6283fd58978e8634af5c6
Prawee Wongsa <prawee@hotmail.com>
2021-10-03 02:01:24 +0700

    test using iap

--- App.js
+++ App.js
@@ -1,7 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, Alert, View, SafeAreaView, Button } from 'react-native';
+import { StyleSheet, Text, Alert, View, SafeAreaView, Button, Platform } from 'react-native';
+
+import IAP from 'react-native-iap';
+
+const items = Platform.select({
+  ios: ['rt_699_1m'],
+  android: [''],
+});
 
 export default function App() {
+
+  const [purchased, setPurchase] = useState(false);
+  const [products, setProducts] = useState({});
+
+  useEffect(() => {
+    IAP.initConnection().catch(() => {
+      console.log('error connection to store');
+    }).then(() => {
+      console.log('connected to store...');
+      IAP.getSubscriptions(items).catch(() => {
+        console.log('error finding purchases');
+      }).then((res) => {
+        console.log('got products');
+        setProducts(res);
+        console.log(products);
+      });
+    });
+  }, []);
+
   return (
     <View>
       <Text>I hate programing</Text>
