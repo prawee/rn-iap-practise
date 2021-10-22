4ca41cc2a321fb9cc727cf71635a62e6af10e2ff
Prawee Wongsa <prawee@hotmail.com>
2021-10-22 16:02:56 +0700

    update how to in app purchase

--- App.js
+++ App.js
@@ -1,240 +1,204 @@
+/**
+ * with Euan Morgan
+ * https://www.youtube.com/watch?v=4JLHRV2kiCU
+ */
 import React, {useState, useEffect} from 'react';
 import {
   StyleSheet,
   Text,
   Alert,
   View,
-  SafeAreaView,
-  Platform,
-  ScrollView,
   Button,
+  Platform,
+  Image,
 } from 'react-native';
-import RNIap, {
-  purchaseUpdatedListener,
-  finishTransaction,
-  purchaseErrorListener,
-} from 'react-native-iap';
+
+import IAP from 'react-native-iap';
 
 const items = Platform.select({
-  ios: ['rt_699_1m', 'VinvestorTest001', 'VinvestorTest01', 'c01', 'uc02'],
-  android: ['rt_699_1m', 'VinvestorTest001', 'VinvestorTest01', 'c01', 'uc02'],
+  // ios: ['com.vinvestor.iaptest99'],
+  ios: ['VinvestorTest001', 'VinvestorTest01'],
+  android: [''],
 });
 
+// eslint-disable-next-line no-unused-vars
+let purchaseUpdatedListener;
+// eslint-disable-next-line no-unused-vars
+let purchaseErrorListener;
+
 export default function App() {
-  const [productLists, setProductLists] = useState();
-  const [receipt, setReceipt] = useState('');
-  const [availableItemsMessage, setAvailableItemsMessage] = useState('');
+  const [purchased, setPurchased] = useState(false);
+  const [products, setProducts] = useState({});
+  const [checking, setChecking] = useState(false);
 
-  let purchaseUpdateSubscription;
-  let purchaseErrorSubscription;
-
-  const initialMount = async () => {
-    try {
-      await RNIap.initConnection().then(async () => {
-        console.log('connected to store...');
-        await RNIap.clearTransactionIOS();
-      });
-      await getItems();
-    } catch (err) {
-      console.warn(err.code, err.message);
-    }
-
-    purchaseUpdateSubscription = purchaseUpdatedListener(async purchase => {
-      console.info('purchase', purchase);
-      const _receipt = purchase.transactionReceipt
-        ? purchase.transactionReceipt
-        : purchase.originalJson;
-      console.info(_receipt);
-
-      if (_receipt) {
+  const validate = async receipt => {
+    setChecking(true);
+    const receiptBody = {
+      'receipt-data': receipt,
+      password: '200749475c574baaace440f831e48469',
+    };
+    const result = await IAP.validateReceiptIos(receiptBody, true)
+      .catch()
+      .then(receiptValidate => {
         try {
-          const ackResult = await finishTransaction(purchase);
-          console.info('ackResult', ackResult);
-        } catch (ackErr) {
-          console.warn('ackErr', ackErr);
+          // console.log(receiptValidate);
+          const renewalHistory = receiptValidate.latest_receipt_info;
+          // console.log('renewWalHistory ', renewWalHistory);
+          const expiration =
+            renewalHistory[renewalHistory.length - 1].expires_date_ms;
+          let expired = Date.new() > expiration;
+          if (!expired) {
+            setPurchased(true);
+          } else {
+            Alert.alert(
+              'Purchase Expired',
+              'Your subscription has expired, please resubscribe to continue using app',
+            );
+          }
+          setChecking(false);
+        } catch (error) {
+          console.log('validateReceiptIos failed.');
         }
-        setReceipt(_receipt);
-      }
-    });
-
-    purchaseErrorSubscription = purchaseErrorListener(error => {
-      console.log('purchaseErrorListener', error);
-      Alert.alert('purchase error', JSON.stringify(error));
-    });
-
-    RNIap.purchaseUpdatedListener(purchase => {
-      console.log('purchase2 ', purchase); // undefined
-    });
-  };
-
-  const getItems = async () => {
-    try {
-      const products = await RNIap.getProducts(items);
-      console.log('getItems(ok) ', products);
-      setProductLists(products);
-    } catch (err) {
-      console.warn('getItems(err) ', err.code, err.message);
-    }
-  };
-
-  const getSubscriptions = async () => {
-    try {
-      const subscriptions = await RNIap.getSubscriptions(items);
-      console.log('getSubscriptions(ok) ', subscriptions);
-      setProductLists(subscriptions);
-    } catch (err) {
-      console.warn('getSubscriptions(err) ', err.code, err.message);
-    }
-  };
-
-  const getAvailablePurchases = async () => {
-    try {
-      console.info(
-        'Get available purchases (non-consumable or unconsumed consumable)',
-      );
-      const purchases = await RNIap.getAvailablePurchases();
-      console.info('getAvailablePurchases(ok) ', purchases);
-      if (purchases && purchases.length > 0) {
-        setAvailableItemsMessage(`Got ${purchases.length} items.`);
-        setReceipt(purchases[0].transactionReceipt);
-      }
-    } catch (err) {
-      console.warn('getAvailablePurchases(err) ', err.code, err.message);
-      Alert.alert(err.message);
-    }
-  };
-
-  const requestPurchase = async sku => {
-    try {
-      RNIap.requestPurchase(sku);
-    } catch (err) {
-      console.warn(err.code, err.message);
-    }
-  };
-
-  const requestSubscription = async sku => {
-    try {
-      RNIap.requestSubscription(sku);
-    } catch (err) {
-      Alert.alert(err.message);
-    }
-  };
-
-  const willUnmount = () => {
-    if (purchaseUpdateSubscription) {
-      purchaseUpdateSubscription.remove();
-      purchaseUpdateSubscription = null;
-    }
-    if (purchaseErrorSubscription) {
-      purchaseErrorSubscription.remove();
-      purchaseErrorSubscription = null;
-    }
-    RNIap.endConnection();
+      });
   };
 
   useEffect(() => {
-    // initialIAP();
-    initialMount();
+    IAP.initConnection()
+      .catch(() => {
+        console.log('error connecting to store');
+      })
+      .then(() => {
+        console.log('connected to store...');
+        IAP.getSubscriptions(items)
+          .catch(() => {
+            console.log('error finding purchases');
+          })
+          .then(res => {
+            console.log('got products ', res);
+            setProducts(res);
+            console.log(products);
+          });
 
-    return () => willUnmount();
+        IAP.getPurchaseHistory()
+          .catch(() => {
+            console.log('nothing getPurchaseHistory');
+            setChecking(false);
+          })
+          .then(res => {
+            console.log('getPurchaseHistory ', res);
+            const receipt = res[res.length - 1].transactionReceipt;
+            if (receipt) {
+              validate(receipt);
+            }
+          });
+      });
+
+    purchaseErrorListener = IAP.purchaseErrorListener(error => {
+      if (error.responseCode === '2') {
+        // user cancelled
+      } else {
+        Alert.alert(
+          'Error',
+          'There has been an error with your purchase, error code =' +
+            error.code,
+        );
+      }
+    });
+
+    purchaseUpdatedListener = IAP.purchaseUpdatedListener(purchase => {
+      try {
+        const receipt = purchase.transactionReceipt;
+        // console.log(receipt);
+        // setPurchased(true);
+        // validate receipt
+        if (receipt) {
+          validate(receipt);
+        }
+      } catch (error) {
+        // test
+      }
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return (
-    <SafeAreaView>
+  if (checking) {
+    return (
       <View style={styles.container}>
-        <View style={styles.header}>
-          <Text style={styles.headerTxt}>RNIap Test</Text>
-        </View>
-        <View>
-          <ScrollView>
-            <Button
-              onPress={() => getAvailablePurchases()}
-              title="Get available purchases"
-            />
-
-            <Text>{availableItemsMessage}</Text>
-
-            <Text>{receipt && receipt.substring(0, 100)}</Text>
-
-            <Button onPress={() => getItems()} title="Get Products" />
-            <Button
-              onPress={() => getSubscriptions()}
-              title="Get Subscriptions"
-            />
-            <Text>{productLists && productLists.length}</Text>
-            {productLists &&
-              productLists.map((product, i) => {
-                return (
-                  <View key={i}>
-                    <Text>{JSON.stringify(product)}</Text>
-                    <Button
-                      // onPress={(): void => this.requestPurchase(product.productId)}
-                      onPress={() => requestSubscription(product.productId)}
-                      title="Request subscription for above product"
-                    />
-                    <Button
-                      onPress={() => requestPurchase(product.productId)}
-                      // onPress={() => requestSubscription(product.productId)}
-                      title="Request purchase for above product"
-                    />
-                  </View>
-                );
-              })}
-          </ScrollView>
-        </View>
+        <Text style={styles.text}>Checking for previous purchase...</Text>
       </View>
-    </SafeAreaView>
-  );
+    );
+  } else {
+    if (purchased) {
+      return (
+        <View style={styles.container}>
+          <Text style={styles.text}>Welcome to the app</Text>
+          <Image style={styles.image} source={require('./dog.jpeg')} />
+        </View>
+      );
+    } else {
+      if (products.length > 0) {
+        return (
+          <View style={styles.container}>
+            <Text style={styles.text}>{products[0].title}</Text>
+            <Text style={styles.text}>1 Month {products[0].localizedPrice}</Text>
+            <View style={styles.divine} />
+            <View style={styles.features}>
+              <Text style={styles.text}>Features:</Text>
+              <Text style={styles.smallText}>
+                {'\u2B24'} Ad-free access to the entire app {'\n'}
+                {'\u2B24'} 1000 wizard tokens {'\n'}
+                {'\u2B24'} legendary dragon skin {'\n'}
+              </Text>
+            </View>
+            <Button
+              title="Purchase"
+              onPress={() => {
+                console.log('Pressed');
+                IAP.requestSubscription(products[0].productId);
+              }}
+            />
+          </View>
+        );
+      } else {
+        return (
+          <View style={styles.container}>
+            <Text style={styles.text}>Fetching products please wait...</Text>
+          </View>
+        );
+      } // end products
+    } // end purchased
+  } // end checking
 }
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
+    flex: 1,
+    backgroundColor: '#000',
     alignItems: 'center',
-    // marginTop: Platform.select({
-    //   ios: 0,
-    //   android: 24,
-    // }),
-    // paddingTop: Platform.select({
-    //   ios: 0,
-    //   android: 24,
-    // }),
-    // backgroundColor: 'green',
-  },
-  header: {
-    flex: 20,
-    flexDirection: 'row',
-    alignSelf: 'stretch',
     justifyContent: 'center',
-    alignItems: 'center',
   },
-  headerTxt: {
-    fontSize: 26,
-    color: 'green',
-  },
-  content: {
-    flex: 80,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignSelf: 'stretch',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-  btn: {
-    height: 48,
-    width: 240,
-    alignSelf: 'center',
-    backgroundColor: '#00c40f',
-    borderRadius: 0,
-    borderWidth: 0,
-  },
-  txt: {
-    fontSize: 16,
+  text: {
     color: 'white',
+    fontSize: 30,
+    paddingBottom: 10,
+  },
+  smallText: {
+    color: 'white',
+    fontSize: 16,
+  },
+  features: {
+    backgroundColor: 'rgba(150, 150, 150, 0.25)',
+    borderRadius: 10,
+    padding: 10,
+    marginTop: 13,
+  },
+  divine: {
+    height: 4,
+    width: 50,
+    backgroundColor: 'white',
+  },
+  image: {
+    with: 150,
+    // resizeMode: 'center',
   },
 });
